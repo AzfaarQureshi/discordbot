784b3118ff03ad7341c08c3ae2a86db5fba9fbca
Azfaar Qureshi <azfaar.qureshi@gmail.com>
2017-07-25 17:19:18 -0400

    importing commands from event based to command based system

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import discord
 import asyncio
-import logging
 from discord.ext import commands
+import logging
 
-logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
-#client = discord.Client()
 
 description = '''Music and Memes, though it has a bunch of other random functions too'''
 
@@ -18,32 +15,55 @@ bot = commands.Bot(command_prefix='//', description=description)
 
 @bot.event
 async def on_ready():
-    print('Instance running')
-    print('instance username:' + bot.user.name)
-    print('instance userid: ' + bot.user.id)
-    print('------')
+	print('Instance running')
+	print('instance username:' + bot.user.name)
+	print('instance userid: ' + bot.user.id)
+	print('------')
 
 @bot.command()
-async def wake(message):
-    if message.content.startswith('wake'):
-        counter = 0
-        tmp = await bot.send_message(message.channel, 'Calculating messages...')
-        async for log in bot.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
-        await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith(bot_prefix+'sleep'):
-        await asyncio.sleep(5)
-        await bot.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith(bot_prefix+'greet'):
-        await bot.send_message(message.channel, 'hello!')
-        await bot.change_presence(game=discord.Game(name='with your mom'))
-       
-@bot.event
-async def on_message_edit(before, after):
-    if before.author.bot == False:
-        await bot.send_message(before.channel, 'oi! change that back')
-        await bot.send_message(before.channel, 'this is what is was before: ' + before.content)
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
 
 bot.run('MzM5Mjc1NDMxNTUzMjY5NzYw.DFjtMw.07IyneSoNDq2knFJ0jpGNv3pAvg')
